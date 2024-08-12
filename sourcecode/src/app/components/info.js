7728cf5a18b1f861b57394cb80cd9e8e9608885e
Dheeraj <dheerajgoud28@gmail.com>
2024-08-12 15:44:17 -0400

    homepage edits

--- sourcecode/src/app/components/info.js
+++ sourcecode/src/app/components/info.js
@@ -1,21 +1,32 @@
 "use client";
 import React from 'react';
+import { Button } from '@mui/material';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import newimg from '/public/newimg.png';
 export default function Info() {
+    const handleGithub = () => {
+        window.open('https://github.com/PixelPerfectDevs/Tranquil-Garden.git', '_blank');
+    }
     return (
         <div style={{ 
             position: 'fixed', 
-            top: '50%', 
-            left: '50%', 
-            width: '100vw', // Set width to 100% of the viewport width
-            height: '100vh',
-            transform: 'translate(-50%, -50%)', 
+            top: 0, 
+            left: 0, 
+            width: '100vw', 
+            height: '100vh', 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center',
             fontSize: '2rem', 
-            // color: 'black', // Changed text color to black for contrast
-            color: 'white', // Set background color to white
-            padding: '20px', // Added padding for better visual appearance
-            borderRadius: '8px' // Optional: Added border radius for a softer look
+            color: 'white', 
+            padding: '20px', 
+            borderRadius: '8px',
+            flexDirection: 'column', 
         }}>
-          Link to github:
+            <iframe id="player" type="text/html" width="640" height="390"
+            src="https://www.youtube.com/embed/o17MF9vnabg"
+            frameborder="0" allowfullscreen></iframe>
+            <Button onClick={handleGithub} sx={{color:'white'}}><img src='https://firebasestorage.googleapis.com/v0/b/tranquil-garden-09.appspot.com/o/newimg.png?alt=media&token=a52f6418-b6c3-4934-9027-17ebfc9facf9' style={{width:'150px'}}/><ArrowOutwardIcon sx={{fontSize: '25px', marginLeft:'-15px'}}/></Button>
         </div>
     );
 }
